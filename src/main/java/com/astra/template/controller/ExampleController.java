7f9e0dd02db9ee467935b3dc65c54dcb55ad70a4
Guilherme <guigomes.ti@gmail.com>
2023-11-14 14:21:55 -0400

    ajuste tests

--- src/main/java/com/astra/template/controller/ExampleController.java
+++ src/main/java/com/astra/template/controller/ExampleController.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.astra.template.dto.ExampleDTO;
@@ -12,6 +13,7 @@ import com.astra.template.dto.ExampleDTO;
 import lombok.RequiredArgsConstructor;
 
 @RestController
+@RequestMapping("v1")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequiredArgsConstructor
 public class ExampleController {
@@ -19,9 +21,9 @@ public class ExampleController {
 	@GetMapping("/")
 	public ResponseEntity<ExampleDTO> index() throws IOException {
 
-		ExampleDTO oTesteResponse = new ExampleDTO();
-		oTesteResponse.setData("Hello Word");
-		return ResponseEntity.ok(oTesteResponse);
+		ExampleDTO oExampleDTO = new ExampleDTO();
+		oExampleDTO.setData("Hello Word");
+		return ResponseEntity.ok(oExampleDTO);
 	}
 
 }
